74b81b6a27d25aad0d394952a9d2b9fe7e4a1c5f
Qube <k.twerdochlib@gmail.com>
2023-01-28 18:27:37 +0100

    up

--- src/index.js
+++ src/index.js
@@ -5,50 +5,132 @@ import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// zmienne
+
 const form = document.querySelector('#search-form');
+const input = document.querySelector('input');
 const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
 
-// largeImageURL - link do dużego obrazka.
+let page = 1;
+const perPage = 40;
+let value = '';
+let totalPages;
 
-const getData = async output => {
-  const res = await axios.get(
-    `https://pixabay.com/api/?key=33147490-9fc73efc70912b9906c0b3bde&q=${output}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  return res.data;
-};
+// lightbox
+
+let lightbox = new SimpleLightbox('.bigPhoto', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// event listener
 
 form.addEventListener('submit', eventHandler);
+loadBtn.addEventListener('click', eventLoader);
+form.addEventListener('keydown', clear);
+loadBtn.style.display = 'none';
 
-function eventHandler(event) {
+// funkcja fetch API
+
+async function fetchImg() {
+  try {
+    const res = await axios.get(
+      `https://pixabay.com/api/?key=33147490-9fc73efc70912b9906c0b3bde&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    return res;
+  } catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure('Something went wrong');
+  }
+}
+
+// funkcje event handlery
+
+async function eventHandler(event) {
+  loadBtn.style.display = 'none';
   event.preventDefault();
-  const name = event.target[0].value;
-  getData(name).then(res => {
-    renderGallery(res);
+  gallery.innerHTML = '';
+  value = input.value;
+  fetchImg(value).then(res => {
+    if (value === '') {
+      return Notiflix.Notify.warning(
+        `Pleas fill search field with your request.`
+      );
+    }
+    totalPages = Math.ceil(res.data.totalHits / perPage);
+    if (res.data.totalHits > 0) {
+      renderGallery(res);
+      loadBtn.style.display = 'block';
+      page += 1;
+      return Notiflix.Notify.info(
+        `Hooray! We found ${res.data.totalHits} images.`
+      );
+    }
+    if (res.data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+    }
   });
 }
 
+function eventLoader() {
+  fetchImg()
+    .then(res => {
+      renderGallery(res);
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure('Something went wrong');
+    });
+  page += 1;
+  if (page > totalPages) {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+}
+
+// funkcja render
+
 function renderGallery(res) {
-  const imgCard = res.hits
+  const imgCard = res.data.hits
     .map(el => {
       return `<div class="photo-card">
-    <img src=${el.webformatURL} alt="${el.tags}" loading="lazy" />
+      <a class="bigPhoto" href=${el.largeImageURL}><img src=${el.webformatURL} alt="${el.tags}" loading="lazy" /></a>
     <div class="info">
       <p class="info-item">
-        <b>Likes ${el.likes}</b>
+        <b>Likes<span class=center> ${el.likes}</span></b>
       </p>
       <p class="info-item">
-        <b>Views ${el.views}</b>
+        <b>Views<span class=center> ${el.views}</span></b>
       </p>
       <p class="info-item">
-        <b>Comments ${el.comments}</b>
+        <b>Comments <span class=center>${el.comments}</span></b>
       </p>
       <p class="info-item">
-        <b>Downloads ${el.downloads}</b>
+        <b>Downloads <span class=center>${el.downloads}</span></b>
       </p>
     </div>
     </div>
     `;
     })
     .join('');
-  gallery.innerHTML = imgCard;
+  gallery.insertAdjacentHTML('beforeend', imgCard);
+  lightbox.refresh();
+}
+
+//funkcje czyszcząca
+
+function clearHTML() {
+  gallery.innerHTML = '';
+}
+
+function clear(event) {
+  const key = event.key;
+  if (key === 'Backspace' || key === 'Delete') {
+    loadBtn.style.display = 'none';
+    return clearHTML();
+  }
 }
